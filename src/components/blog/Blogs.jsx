4ebe58b16a26cd4677a9f8efc5e319b17975a6af
Ali Ghaffari <aligh71@yahoo.com>
2024-02-25 21:04:15 +0330

    arrange the blogs section and making it responsive

--- src/components/blog/Blogs.jsx
+++ src/components/blog/Blogs.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Grid } from "@mui/material";
 import { useQuery } from "@apollo/client";
 import { GET_BLOGS_INFO } from "../../qraphql/queries";
+import CardEL from "../../shared/CardEL";
 
 function Blogs() {
   const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
-  console.log({ loading, data, errors });
-  return <div>Blogs</div>;
+
+  if (loading) return <h4>loading</h4>;
+  if (errors) return <h4>an error occured!</h4>;
+  return (
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+          <CardEL {...post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default Blogs;
---
+++ src/shared/CardEL.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function CardEL(props) {
+  console.log(props);
+  return <div>CardEL</div>;
+}
+
+export default CardEL;
