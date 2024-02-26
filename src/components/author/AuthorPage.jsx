b849237efbfeca606ab2c66bf60e3563408938ea
Ali Ghaffari <aligh71@yahoo.com>
2024-02-26 11:49:01 +0330

    getting the data of an author using slug

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { GET_AUTHOR_INFO } from "../../qraphql/queries";
 
 function AuthorPage() {
-  return (
-    <div>AuthorPage</div>
-  )
+  const { slug } = useParams();
+  const { loading, data, errors } = useQuery(GET_AUTHOR_INFO, {
+    variables: { slug },
+  });
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (errors) return <h3>An error occured!</h3>;
+  console.log(data);
+  return <div>AuthorPage</div>;
 }
 
-export default AuthorPage
+export default AuthorPage;
