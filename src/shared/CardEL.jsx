0326f50622c0d2bcad7bb247c25ab1468bfbabed
Ali Ghaffari <aligh71@yahoo.com>
2024-02-26 13:02:08 +0330

    create author details page dynamically

--- src/shared/CardEL.jsx
+++ src/shared/CardEL.jsx
@@ -15,14 +15,17 @@ import { Link } from "react-router-dom";
 function CardEL({ title, slug, coverPhoto, author }) {
   return (
     <Card sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}>
-      <CardHeader
-        avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
-        title={
-          <Typography component="p" variant="p" color="text.secondary">
-            {author.name}
-          </Typography>
-        }
-      />
+      {author && (
+        <CardHeader
+          avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
+          title={
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.name}
+            </Typography>
+          }
+        />
+      )}
+
       <CardMedia
         component="img"
         height="194"
@@ -40,7 +43,9 @@ function CardEL({ title, slug, coverPhoto, author }) {
       </CardContent>
       <Divider variant="middle" sx={{ margin: "10px" }} />
       <CardActions>
-        <Link to={`/blogs/${slug}`} style={{textDecoration:"none",width:"100%"}}>
+        <Link
+          to={`/blogs/${slug}`}
+          style={{ textDecoration: "none", width: "100%" }}>
           <Button
             variant="outlined"
             size="small"
