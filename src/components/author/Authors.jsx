58b2bbc420996562e49cbba32c6297e6f9a8aede
Ali Ghaffari <aligh71@yahoo.com>
2024-02-26 10:07:24 +0330

    create CardEL component & get authors data

--- src/components/author/Authors.jsx
+++ src/components/author/Authors.jsx
@@ -1,6 +1,15 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { GET_AUTHORS_INFO } from "../../qraphql/queries";
 
 function Authors() {
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+  console.log({ loading, data, errors });
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (errors) return <h3>An error occured!</h3>;
+
   return <div>Authors</div>;
 }
 
