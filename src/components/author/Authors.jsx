001e64846c89fb9d953fd43408546f5e92e0ed4f
Ali Ghaffari <aligh71@yahoo.com>
2024-02-26 10:51:24 +0330

    show authors on the main page

--- src/components/author/Authors.jsx
+++ src/components/author/Authors.jsx
@@ -1,16 +1,49 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_AUTHORS_INFO } from "../../qraphql/queries";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Authors() {
   const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
-  console.log({ loading, data, errors });
 
   if (loading) return <h3>Loading...</h3>;
 
   if (errors) return <h3>An error occured!</h3>;
 
-  return <div>Authors</div>;
+  const { authors } = data;
+  return (
+    <Grid
+      container
+      sx={{
+        boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px",
+        borderRadius: 4,
+      }}>
+      {authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid item xs={12} padding={2}>
+            {/* <Link
+            to={`/authors/${author.slug}`}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+            }}> */}
+            <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.name}
+            </Typography>
+            {/* </Link> */}
+          </Grid>
+          {index !== authors.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
 }
 
 export default Authors;
