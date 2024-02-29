a4312424ae25cc9e7de828af8b002d49689b5728
Ali Ghaffari <aligh71@yahoo.com>
2024-02-29 11:16:14 +0330

    articles details page

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,19 +1,66 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { GET_BLOG_INFO } from "../../qraphql/queries";
 import Loader from "../../shared/Loader";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import sanitizeHtml from "sanitize-html";
 
 function BlogPage() {
   const { slug } = useParams();
   const { loading, data, errors } = useQuery(GET_BLOG_INFO, {
     variables: { slug },
   });
+  const navigate = useNavigate();
 
   if (loading) return <Loader />;
 
   if (errors) return <h3>An error occured!</h3>;
-  return <div>BlogPage</div>;
+  return (
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12} mt={9} display="flex" justifyContent="space-between">
+          <Typography
+            component="h2"
+            variant="h4"
+            color="primary"
+            fontWeight={700}>
+            {data.post.title}
+          </Typography>
+          <ArrowBackRoundedIcon onClick={() => navigate(-1)} />
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <img
+            src={data.post.coverPhoto.url}
+            alt={data.post.slug}
+            width="100%"
+            style={{ borderRadius: 15 }}
+          />
+        </Grid>
+        <Grid item xs={12} mt={7} display="flex" alignItems="center">
+          <Avatar
+            src={data.post.author.avatar.url}
+            sx={{ width: 80, height: 80, marginLeft: 2 }}
+          />
+          <Box component="div">
+            <Typography component="p" variant="h5" fontWeight={700}>
+              {data.post.author.name}
+            </Typography>
+            <Typography component="p" variant="p" color="text.secondary">
+              {data.post.author.field}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} mt={5}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHtml(data.post.content.html),
+            }}></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default BlogPage;
